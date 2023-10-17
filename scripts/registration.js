39fc2d848f3d19b27850e5a1bf14ad95d12974c3
kovaaalex <kovalenko.alex04@gmail.com>
2023-10-17 13:48:11 +0300

    24-registrationWidthAdaptive

--- scripts/registration.js
+++ scripts/registration.js
@@ -2,14 +2,12 @@ const radios = document.querySelectorAll('.radio')
 for (let i = 0; i< radios.length; i++)
 {
     let r = radios[i]
-    r.addEventListener("click", function()
+    r.addEventListener("click", () =>
     {
-        if(r.checked === true)
-            r.addEventListener("click", function()
+        if(r.checked)
+            r.addEventListener("click", ()=>
             {
-                if(r.checked === true)
-                    r.checked = false  
-                else r.checked = true;
+                r.checked ? r.checked = false :  r.checked = true;
             }
         )
     }
